53b49a19ca2b4fd645f09377252492690f79f619
Rudra <rudrahimangshoo2020@gmail.com>
2024-05-02 13:18:58 +0600

    count-digit

---
+++ algo/basic/math/countDigit.js
@@ -0,0 +1,30 @@
+// Function to count the number
+// of digits in an integer 'n'.
+function countDigits(n) {
+    // Initialize a counter variable
+    // 'cnt' to store the count of digits.
+    let count = 0;
+    // While loop iterates until 'n'
+    // becomes 0 (no more digits left).
+    while (n > 0) {
+        // Increment the counter
+        // for each digit encountered.
+        count = count + 1;
+        // Divide 'n' by 10 to
+        // remove the last digit.
+        n = Math.floor(n / 10);
+    }
+    // Return the
+    // count of digits.
+    return cnt;
+}
+
+console.log(countDigits(2343353))
+
+function main(){
+    let N = 344535;
+    console.log("N"+ N);
+    let digits = countDigits(N)
+    console.log("number of digits " + digits)
+}
+main()
