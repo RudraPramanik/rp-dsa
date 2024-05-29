0f490f9132db73e90072685227f6a188679c770d
Rudra <rudrahimangshoo2020@gmail.com>
2024-05-29 12:14:24 +0600

    pattern

---
+++ algo/basic/pattern/pattern.js
@@ -0,0 +1,26 @@
+function printPattern(n: number): void {
+    let str = '';
+
+    // Upper part of the pattern including the middle row
+    for (let i = 1; i <= n; i++) {
+        for (let j = 1; j <= i; j++) {
+            str += '*';
+        }
+        str += '\n';
+    }
+
+    // Lower part of the pattern excluding the middle row
+    for (let i = n - 1; i >= 1; i--) {
+        for (let j = 1; j <= i; j++) {
+            str += '*';
+        }
+        str += '\n';
+    }
+
+    // Print the complete pattern
+    console.log(str);
+}
+
+// Example usage:
+const n = 3;
+printPattern(n);
