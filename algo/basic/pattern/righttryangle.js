2c0d9e1ed8f25ed6ab485610d362dd01cd16db00
Rudra <rudrahimangshoo2020@gmail.com>
2024-05-29 12:18:49 +0600

    right angel

---
+++ algo/basic/pattern/righttryangle.js
@@ -0,0 +1,14 @@
+// *
+// **
+// ***
+// ****
+// *****
+
+function printRightAngledTriangle(n) {
+    for (let i = 1; i <= n; i++) {
+        console.log('*'.repeat(i));
+    }
+}
+
+// Example usage:
+printRightAngledTriangle(5);
