0e6d707b05178193e1ab379bdbacefb99ddab08b
Rudra <rudrahimangshoo2020@gmail.com>
2024-06-25 15:53:39 +0600

    array

---
+++ algo/array/2small2large.js
@@ -0,0 +1,14 @@
+function arrsmall(arr){
+    if (arr.length === 0 || arr.length === 1){
+        console.log(-1 + " "+ -1)
+        return
+    }
+    arr.sort((a,b)=> a-b)
+    let small = arr[1]
+    let large = arr[arr.length-2]
+    console.log("Second smallest is " + small);
+  console.log("Second largest is " + large);
+}
+
+const arr = [1, 2, 4, 6, 7, 5];
+arrsmall(arr);
