c99ef28684ef207d17495705b7196622cdc206ee
Rudra Pramanik <rudrahimangshoo2020@gmail.com>
2024-09-01 13:00:40 +0600

    atoi-step-2

--- algo/string/atoi.ts
+++ algo/string/atoi.ts
@@ -9,3 +9,20 @@
 // Conversion: Read the integer by skipping leading zeros until a non-digit character is encountered or the end of the string is reached. If no digits were read, then the result is 0.
 // Rounding: If the integer is out of the 32-bit signed integer range [-231, 231 - 1], then round the integer to remain in the range. Specifically, integers less than -231 should be rounded to -231, and integers greater than 231 - 1 should be rounded to 231 - 1.
 // Return the integer as the final result.
+
+function myAtoi(s: string): number {
+  //ignore leading white space
+  s = s.trimStart();
+  if (!s) {
+    return 0;
+  }
+  //determine the sign
+  let sign = 1;
+  let index = 0;
+  if (s[0] === '-') {
+    sign = -1;
+    index++;
+  } else if (s[0] === '+') {
+    index++;
+  }
+}
