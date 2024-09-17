2bdc30e5e4d7544e14d9ed72ea91f2cdc9996846
Rudra Pramanik <rudrahimangshoo2020@gmail.com>
2024-09-17 14:30:14 +0600

    find

--- algo/linked/nodeCreate.ts
+++ algo/linked/nodeCreate.ts
@@ -1,9 +1,22 @@
 class node<T> {
-  element: T; // Stores the data of the node
-  next: node<T> | null; // Points to the next node in the list or null if it's the last node
-
+  element: T;
+  next: node<T> | null;
   constructor(element: T) {
     this.element = element;
     this.next = null;
+  }
+}
+
+class list {
+  constructor() {
+    this.head = new node('head'); //initialize with head node
+  }
+  //find a node
+  find(element) {
+    let currentNode = this.head;
+    while (currentNode !== null && currentNode.element !== element) {
+      currentNode = currentNode.next;
+    }
+    return currentNode;
   }
 }
