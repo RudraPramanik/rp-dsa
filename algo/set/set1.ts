4254ab68ea90b1eac58fdb4e0031ed743d819a11
Rudra Pramanik <rudrahimangshoo2020@gmail.com>
2024-10-29 13:45:06 +0600

    set-operations

--- algo/set/set1.ts
+++ algo/set/set1.ts
@@ -2,3 +2,9 @@ const set = new Set<number>();
 set.add(1);
 set.add(2);
 set.add(3);
+
+set.add(4);
+set.delete(2); // removes 2 from the set
+
+console.log(set.has(3)); // true
+console.log(set.has(2)); // false
