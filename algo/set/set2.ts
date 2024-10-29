aa67d5bccf77aadd32e85e60636136b4f7d07d9d
Rudra Pramanik <rudrahimangshoo2020@gmail.com>
2024-10-29 13:47:25 +0600

    set-intersection

--- algo/set/set2.ts
+++ algo/set/set2.ts
@@ -3,3 +3,6 @@ const setB = new Set([3, 4, 5]);
 
 const union = new Set([...setA, ...setB]);
 console.log(union); // Set { 1, 2, 3, 4, 5 }
+
+const intersection = new Set([...setA].filter((x) => setB.has(x)));
+console.log(intersection); // Set { 3 }
