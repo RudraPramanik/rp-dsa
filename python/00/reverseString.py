7883c19978ad5424847c1b0c5438b122def872ee
Rudra Pramanik <rudrahimangshoo2020@gmail.com>
2026-02-02 14:11:40 +0600

    py-00

---
+++ python/00/reverseString.py
@@ -0,0 +1,23 @@
+print("FILE IS RUNNING")
+
+def reverseString(s):
+    left, right = 0, len(s) - 1
+    while left < right:
+        s[left], s[right] = s[right], s[left]
+        left += 1
+        right -= 1
+    return s
+
+print(reverseString(["H", "a", "n", "n", "a", "h"]))
+
+def reverseString2(s):
+    arr = list(s)
+    left, right = 0, len(arr)-1
+    while left < right:
+        arr[left], arr[right]= arr[right], arr[left]
+        left +=1
+        right -=1
+
+        return "".join(arr)
+
+print(reverseString2("hello"))
