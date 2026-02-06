3222c533dd00c30b2528661797fc9e3fa12e1b67
Rudra Pramanik <rudrahimangshoo2020@gmail.com>
2026-02-06 14:16:17 +0600

    fibo

--- python/00/isPalindrome.py
+++ python/00/isPalindrome.py
@@ -2,9 +2,9 @@
 def isPalindrome(s):
     left, right = 0,len(s)-1
     while left < right:
-        while left < right and not s[left].isalnum():
+        # while left < right and not s[left].isalnum():
             left +=1
-        while left < right and not s[right].isalnum():
+        # while left < right and not s[right].isalnum():
             right -= 1
         
         if(s[left].lower() != s[right].lower()):
