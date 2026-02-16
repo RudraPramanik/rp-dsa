c4289afb162349899612cd34b7d561ddd27c13a3
Rudra Pramanik <rudrahimangshoo2020@gmail.com>
2026-02-16 17:51:05 +0600

    ispalindrome

--- python/00/isPalindrome.py
+++ python/00/isPalindrome.py
@@ -1,21 +1,19 @@
 
 def isPalindrome(s):
-    left, right = 0,len(s)-1
-    while left < right:
-        # while left < right and not s[left].isalnum():
-            left +=1
-        # while left < right and not s[right].isalnum():
+    left , right = 0, len(s)-1
+
+    while left<right :
+        while left<right and not s[left].isalnum():
+            left += 1
+        while left<right and not s[right].isalnum():
             right -= 1
-        
-        if(s[left].lower() != s[right].lower()):
+        if(s[left].lower() != s[right].lower() ):
             return False
         left +=1
         right -=1
     return True
 
-print(isPalindrome("dbab"))
-# two pointer pattern(left, right)
-# while loop as long left left smaller
+print(isPalindrome("abBa"))
 # nested while(edge case should not be number:isalnum()),
 # left increase/ right decrease(if the condition satisfy)
 # inside while looping (if input[left] !== input[right]) =>false
