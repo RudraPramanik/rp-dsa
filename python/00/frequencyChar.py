8543b63fcb5ff414e8b1475608e2269b696c84c5
Rudra Pramanik <rudrahimangshoo2020@gmail.com>
2026-02-16 17:17:22 +0600

    00

--- python/00/frequencyChar.py
+++ python/00/frequencyChar.py
@@ -4,18 +4,19 @@ class Solution:
 
          # Step 1: Count frequency
         for char in stringData:
-            freq[char] = freq.get(char, 0) + 1
+            freq[char] = freq.get(char,0)+1
 
           # Step 2: Find first unique
         for i, char in enumerate(stringData):
             if freq[char] == 1:
                 return i
-        return -1
+                return -1
+        
 
 solution = Solution()
 print(solution.firstUniqChar("leetcode"))
-print(solution.firstUniqChar("loveleetcode"))
-print(solution.uniqueChars("rudra"))
+print(solution.firstUniqChar("loveleetcodve"))
+print(solution.firstUniqChar("rudraa"))
 
 #Frequency HashMap + Second Pass
 # One loop to count, One loop to find
