fc2d51561c028c740f3f37baef38e176a0b7340f
RudraPramanik <rudrahimangshoo2020@gmail.com>
2026-05-15 15:43:22 +0600

    ifelf

--- python/fundamentals/collection.py
+++ python/fundamentals/collection.py
@@ -12,10 +12,10 @@ a, b, *rest = t
 # set — unordered, unique
 s = {1, 2, 3}
 s.add(4)
-s1 & s2   # intersection
-s1 | s2   # union
+# s1 & s2   # intersection
+# s1 | s2   # union
 
 # dict — key→value
 d = {"a": 1, "b": 2}
 d.get("c", 0)        # default=0
-print(d.setdefault("x",[]))
+print(d)
